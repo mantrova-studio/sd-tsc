ece2cb19ecf27819dc12ad62d6cfacfa94988856
mantrova-studio <260282153+mantrova-studio@users.noreply.github.com>
2026-02-28 03:06:03 +0500

    Update zones.js

--- zones/js/zones.js
+++ zones/js/zones.js
@@ -265,7 +265,7 @@
   async function nominatimSearch(query) {
   // Прямоугольник вокруг Оренбурга + район (lon1,lat1,lon2,lat2)
   // запад/юг/восток/север — с запасом
-  const viewbox = [54.60, 51.40, 55.60, 52.20].join(",");
+  const viewbox = [34.60, 31.40, 75.60, 72.20].join(",");
 
   const url =
     "https://nominatim.openstreetmap.org/search" +
