21e8320e0bc0880ed267db13e42bc8ed90f83556
mantrova-studio <260282153+mantrova-studio@users.noreply.github.com>
2026-03-01 01:23:39 +0500

    Update admin.js

--- tsc/js/admin.js
+++ tsc/js/admin.js
@@ -201,10 +201,10 @@ function renderList(){
       </div>
 
       <div class="rowRight">
-        <button class="iconBtn" data-act="edit" title="Редактировать">
+        <button class="iconBtn hasTip" data-act="edit" data-tip="Редактировать">
           <img src="/tsc/assets/icons/edit.svg" alt="edit" />
         </button>
-        <button class="iconBtn delete" data-act="delete" title="Удалить">
+        <button class="iconBtn delete hasTip" data-act="delete" data-tip="Удалить">
           <img src="/tsc/assets/icons/delete.svg" alt="delete" />
         </button>
       </div>
